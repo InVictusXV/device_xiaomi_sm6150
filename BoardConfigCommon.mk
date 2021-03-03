f55537ca013abb9af8827cc1497f215cbad5e8e8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-03 13:18:16 +0100

    sm6150-common: Kill console parameters
    Change-Id: I69a14e00cfc3291175351259f2d8cbd6b0dc827a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,8 +80,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_CLANG_VERSION := playground
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-playground
 
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
-BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
+BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
